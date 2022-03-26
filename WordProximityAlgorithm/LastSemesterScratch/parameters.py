be5365cf04f3e2cd7ee164ef83c45fd3caf0f938
AttackPenguin <denis.e.hudon@gmail.com>
2022-03-26 10:50:02 -0400

    Previous crude version of WordProximityAlgorithm pulled into new project as a starting point for new algorithm.

---
+++ WordProximityAlgorithm/LastSemesterScratch/parameters.py
@@ -0,0 +1,8 @@
+import os.path
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_DIR = os.path.join(CURR_DIR, 'output')
+RESOURCES_DIR = os.path.join(CURR_DIR, 'resources')
+TEXTS_DIR = os.path.join(CURR_DIR, 'texts')
+TOOLS_DIR = os.path.join(CURR_DIR, 'tools')
+pass
---
+++ WordProximityAlgorithm/LastSemesterScratch/resources/source_specific_data.py
@@ -0,0 +1,202 @@
+lord_of_the_rings = {
+    'Character Names': [
+        'frodo',
+        'sam',
+        'gandalf',
+        'aragorn',
+        'pippin',
+        'merry',
+        'gollum',
+        'gimli',
+        'legolas',
+        'faramir',
+        'bilbo',
+        'boromir',
+        'saruman',
+        'strider',
+        'éomer',
+        'théoden',
+        'elrond',
+        'sméagol',
+        'treebeard',
+        'denethor',
+        'sauron',
+        'tom',
+        'éowyn',
+        'galadriel',
+        'lórien',
+        'shadowfax',
+        'beregond',
+        'butterbur',
+        'wormtongue',
+        'isildur',
+        'uglúk'
+    ],
+    'File Names': [
+        'the_fellowship_of_the_ring_trimmed.txt',
+        'the_return_of_the_king_trimmed.txt',
+        'the_two_towers_trimmed.txt'
+    ]
+}
+
+lolita = {
+    'Character Names': [
+        'humbert',
+        'lolita',
+        'dolores',
+        'clare',
+        'charlotte',
+        'annabel',
+        'valeria',
+        'jean',
+        'john',
+        'dick',
+        'rita',
+        'mona',
+        'gaston',
+        'pratt',
+        'ivor',
+        'monique',
+        'shirley',
+        'charlie',
+        'barbara',
+        'vivian',
+        'jack',
+        'frederick',
+        'hh',
+        'lo',
+        'dolly',
+        'valechka',
+        'taximovich'
+    ],
+    'File Names' : [
+        'lolita.txt'
+    ]
+}
+
+pride_and_prejudice = {
+    'Character Names': [
+        'elizabeth',
+        'fitzwilliam',
+        'jane',
+        'charles',
+        'george',
+        'lydia',
+        'collins',
+        'bingley',
+        'catherine',
+        'charlotte',
+        'georgiana',
+        'mary'
+    ],
+    'File Names': [
+        'pride_and_prejudice.txt'
+    ]
+}
+
+# the_accursed = {
+#     'Character Names': [
+#
+#     ],
+#     'File Names': [
+#         'the_accursed.txt'
+#     ]
+# }
+
+the_sound_and_the_fury = {
+    'Character Names': [
+        'jason',
+        'caroline',
+        'candace',
+        'caddy',
+        'benjy',
+        'quentin'
+    ],
+    'File Names': [
+        'the_sound_and_the_fury.txt'
+    ]
+}
+
+the_portrait_of_a_lady = {
+    'Character Names': [
+        'isabel',
+        'gilbert',
+        'merle',
+        'ralph',
+        'warburton',
+        'caspar',
+        'henrietta',
+        'pansy',
+        'edward',
+        'bantling',
+        'gemini'
+    ],
+    'File Names': [
+        'the_portrait_of_a_lady.txt'
+    ]
+}
+
+the_inferno = {
+    'Character Names': [
+        'dante',
+        'virgil',
+        'beatrice',
+        'charon',
+        'paolo',
+        'francesca',
+        'lucifer',
+        'judas',
+        'cassius',
+        'brutus',
+        'minos',
+        'boniface',
+        'farinata',
+        'phlegyas',
+        'filippo',
+        'nessus',
+        'vigna',
+        'geryon',
+        'malacoda',
+        'vanni',
+        'ulysses',
+        'guido',
+        'antaeus',
+        'ugolino',
+        'alberigo',
+        'branca'
+    ],
+    'File Names': [
+        'the_inferno.txt'
+    ]
+}
+
+the_gormenghast_trilogy = {
+    'Character Names': [
+        'titus',
+        'sepulchrave',
+        'gertrude',
+        'fuchsia',
+        'cora',
+        'clarice',
+        'steerpike',
+        'flay',
+        'alfred',
+        'bernard',
+        'irma',
+        'abiatha',
+        'swelter',
+        'nannie',
+        'slagg',
+        'sourdust',
+        'barquentine',
+        'bellgrove',
+        'keda',
+        'muzzlehatch',
+        'juno',
+        'cheeta'
+    ],
+    'File Names': [
+        'the_gormenghast_trilogy.txt'
+    ]
+}
+
