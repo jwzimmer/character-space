1baf3c3eeafae8f195c91433111fecbe08cf4861
jwzimmer <jzimmer1@uvm.edu>
2022-04-03 13:44:39 -0400

    add a chart for svd output; i'm not sure why you have to index into U and V differently when getting them back out of json instead of working with the original array but it looks like it's working?

---
+++ svd_visualizations.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+import pandas as pd
+
+def vector_barchart(vector_names ,vector ,n ,style="by_mag" ,ascending=False):
+    """ vector_names should be the labels for the values in the vector
+        vector should be the vector (ndarray)
+        n should be the number of values you want displayed in the chart
+        style should be the format of the chart
+        ascending=False will be most relevant traits by magnitude,
+        ascending=True will be least relevant traits by magnitude
+
+        Example: vectordf, plotguy = vector_barchart(trait_columns_relabeled,V[2,:],10)"""
+    n=min( n,len( vector_names))
+    vectordf = pd.DataFrame()
+    vectordf["Trait"] = vector_names
+    vectordf["Values"] = vector
+    if style=="by_mag":
+        vectordf["Magnitude"] = vectordf.apply(lambda row: abs(row["Values"]), axis = 1)
+        sorteddf = vectordf.sort_values(by="Magnitude",ascending=ascending)
+        # plotguy = sorteddf.iloc[-2*n:].iloc[::-1]
+        plotguy = sorteddf.iloc[0:2*n]
+    # sns.set(font_scale = 2)
+    sns.barplot(plotguy["Values"],plotguy["Trait"],ci=None)
+    # sns.set(font_scale = 1)
+    plt.show()
+    return vectordf, plotguy
+
+if __name__ == '__main__':
+    with open("/Users/jzimmer1/Documents/GitHub/character-space/V.json") as f:
+        V = json.loads(f.read())
+    data_df = pd.read_json("/Users/jzimmer1/Documents/GitHub/character-space/lsa.json")
+    vector_barchart(data_df.columns, V[0], 15)
