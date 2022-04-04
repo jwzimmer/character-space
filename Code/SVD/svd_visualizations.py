8f51e1eb4b9b71ff730f875c10fece300c4b34f3
AttackPenguin <denis.e.hudon@gmail.com>
2022-04-03 21:09:43 -0400

    .

--- Code/SVD/svd_visualizations.py
+++ Code/SVD/svd_visualizations.py
@@ -3,7 +3,8 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-def vector_barchart(vector_names ,vector ,n ,style="by_mag" ,ascending=False):
+
+def vector_barchart(vector_names, vector, n, style="by_mag", ascending=False):
     """ vector_names should be the labels for the values in the vector
         vector should be the vector (ndarray)
         n should be the number of values you want displayed in the chart
@@ -12,22 +13,25 @@ def vector_barchart(vector_names ,vector ,n ,style="by_mag" ,ascending=False):
         ascending=True will be least relevant traits by magnitude
 
         Example: vectordf, plotguy = vector_barchart(trait_columns_relabeled,V[2,:],10)"""
-    n=min( n,len( vector_names))
+    n = min(n, len(vector_names))
     vectordf = pd.DataFrame()
     vectordf["Trait"] = vector_names
     vectordf["Values"] = vector
-    if style=="by_mag":
-        vectordf["Magnitude"] = vectordf.apply(lambda row: abs(row["Values"]), axis = 1)
-        sorteddf = vectordf.sort_values(by="Magnitude",ascending=ascending)
+    if style == "by_mag":
+        vectordf["Magnitude"] = vectordf.apply(lambda row: abs(row["Values"]),
+                                               axis=1)
+        sorteddf = vectordf.sort_values(by="Magnitude", ascending=ascending)
         # plotguy = sorteddf.iloc[-2*n:].iloc[::-1]
-        plotguy = sorteddf.iloc[0:2*n]
+        plotguy = sorteddf.iloc[0:2 * n]
     # sns.set(font_scale = 2)
-    sns.barplot(plotguy["Values"],plotguy["Trait"],ci=None)
+    sns.barplot(plotguy["Values"], plotguy["Trait"], ci=None)
     # sns.set(font_scale = 1)
     plt.show()
     return vectordf, plotguy
 
+
 if __name__ == '__main__':
     V = np.load("V.npy")
-    data_df = pd.read_json("/Users/jzimmer1/Documents/GitHub/character-space/lsa.json")
-    vector_barchart(data_df.columns, V[0,:], 15)
+    data_df = pd.read_json(
+        "/Users/jzimmer1/Documents/GitHub/character-space/lsa.json")
+    vector_barchart(data_df.columns, V[0, :], 15)
