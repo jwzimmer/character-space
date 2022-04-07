3921163fce81027bb555ea2b8a2aa75143ef7961
jwzimmer <jzimmer1@uvm.edu>
2022-04-07 13:18:43 -0400

    regex to split on ., !, ? for lsa

--- Code/LSAEmbedding/lsa.py
+++ Code/LSAEmbedding/lsa.py
@@ -22,7 +22,8 @@ def run_LSA(text, character_names, prefix_list):
     # following steps in https://towardsdatascience.com/latent-semantic-analysis-deduce-the-hidden-topic-from-the-document-f360e8c0614b
     text = text.lower()
     text = pad_prefixes(text, prefix_list)
-    documents = text.split(".|!|?")
+    #documents = text.split(".|!|?")
+    documents = re.split(r'(\.|\!|\?)', text)
     df = pd.DataFrame()
     df["documents"] = documents
     # remove special characters
